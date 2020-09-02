fe8f61a2aea863f89b5fcf6b624153c476516706
devebrahim <sadeghiebrahim.dev@gmail.com>
2020-09-02 19:06:54 +0430

    create part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, ApplicationRef, assertPlatform } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'JustToDo';
+constructor(){}
+ngOnInit(){
+  }
+  
+ fas() {
+  var x = document.getElementsByClassName("input_form")[0];
+  var y=document.getElementsByClassName("addet_form")[0].innerHTML;
+
+  alert(x)
+  alert(y)
+}
 }
